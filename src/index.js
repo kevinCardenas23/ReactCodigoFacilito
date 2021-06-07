2a99ffaa74ea9250c2dc6a9cba1de8a8e5e76ede
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2021-06-07 14:21:48 -0500

    Condicionales

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// function SaludarEnIdiomas(idioma){
+//   if(idioma==='en') {
+//     return <span>Hello</span>
+//   }
+//   else{
+//     return <span>Hola</span>
+
+//   }
+// }
+
 const Saludo = () => {
   const nombre = "Kevin";
-  return <p>Hola {`${nombre}`} </p>;
+  const idioma = 'es';
+  //Esto es otra manera de hacer un if 
+  // return <div> 
+  //   {
+  //     idioma == 'es' && <p>Hola</p>
+  //   }
+  //   {
+  //     idioma == 'en' && <p>Hello</p>
+  //   }
+  // </div>
+  //Esta es otra manera
+  return <div>
+    {
+      idioma === 'en' ? <p>Hello</p> : <p>Hola</p>
+    }
+  </div>
 }
 
 const App = () => {
