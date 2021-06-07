0081a440dcae26ff856bd9941aec73150a0f842e
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2021-06-07 14:02:08 -0500

    Primer componente creado

--- src/index.js
+++ src/index.js
@@ -18,8 +18,16 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+
+const Saludo = () => {
+  return <p>Hola </p>;
+}
+
 const App = () => {
-  return <h1>Hola mundo</h1>;
+  return <h1>
+    Hola mundo
+    <Saludo/>
+  </h1>;
 };
 
 render(<App />, document.getElementById('react-app'));
