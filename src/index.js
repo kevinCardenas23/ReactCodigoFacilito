df2fbb1e968adf8de6904e8790633bbd4830734f
Kevin Cardenas <kscardenasa2013t@gmail.com>
2021-06-07 17:38:39 -0500

    Se creo un evento

--- src/index.js
+++ src/index.js
@@ -1,42 +1,21 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { useEffect } from 'react/cjs/react.development';
 
-const Button = ()=>{
-  const [conteo, setConteo] = useState(0);
-  // useEffect(()=>{
-  //   console.log("Me ejecute!!!");
-  // })
-  //Se ejecuta varias veces cuando se actualza el componente
+const Saludo = ()=>{
+  const [name, setName] = useState("");
 
-  // useEffect(()=>{
-  //   console.log("Me ejecute!!!");
-  // },[])
-  //Se ejecuta solo al iniciar el componewnte
-  //Se eejcuta solo una vez, sie l componente se actualza esto no, se añade un arreglo al final 
-  
-  useEffect(()=>{
-    console.log("Me ejecute!!!");
-    return () => {
-      console.log("Adios!!!!");
-    }
-  },[]);
-  //Si la funcion retorna otra funcion, esa se ejecuta cuando se elimine el componente
-  
-  return <button onClick={()=>[setConteo(conteo+1)]}> Hacer CLICK JAJAJA</button>
-
+  return (
+    <div>
+      <input type="text" onChange={(ev)=>{setName(ev.target.value)}}/>
+      <p>Hola {name}</p>
+    </div>
+  )
 }
 
 const App = () => {
-  const [showButton, setShowButton] = useState(false);
-  return (
-    <div>
-      <button onClick={ ()=> {setShowButton(false)} }>Eliminar boton</button>
-      <div>
-        {showButton && <Button/>}
-      </div>
-    </div>
-  )
+  return(
+    <Saludo></Saludo>
+  );
 };
 
 render(<App />, document.getElementById('react-app'));
