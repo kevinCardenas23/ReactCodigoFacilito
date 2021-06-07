a154060329a62295720c87b3aaaa8938a6889f45
Kevin Cardenas <kscardenasa2013t@gmail.com>
2021-06-07 16:25:01 -0500

    Se agregaron props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// const App = () => {
-//   return <div>
-    
-//   </div>
-// };
+// class App extends Component{
+//   render(){
+//     return <h1>Hola mundillo!!!</h1>
+//   }
+// }
 
-class App extends Component{
-  render(){
-    return <h1>Hola mundillo!!!</h1>
-  }
+const nombres = [
+  'Uriel',
+  'Pedro',
+  'Juan',
+  'Felipe',
+  'Andres'
+]
+
+// const Saludar = ({nombre, idioma}) =>{
+const Saludar = ({nombre, idioma = 'es'}) =>{
+  const saludo = idioma === 'en' ? 'Hola' : 'Hello';
+  return <p>{saludo} {nombre}</p>
 }
+
+Saludar.defaultProps = {
+  idioma: 'es'
+}
+
+const App = () => {
+  return <div>
+    <Saludar nombre='Kevin' idioma='es'/>
+  </div>
+};
 
 render(<App />, document.getElementById('react-app'));
