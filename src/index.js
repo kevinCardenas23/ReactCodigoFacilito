e8878f5023bb8376524ecf94dd2cf97dbfe050fa
Kevin Cardenas <kscardenasa2013t@gmail.com>
2021-06-07 17:00:52 -0500

    Modos de useEffect

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
+import { useEffect } from 'react/cjs/react.development';
 
 const Button = ()=>{
-  const [conteo, setCounter] = useState(0);
+  const [conteo, setConteo] = useState(0);
+  // useEffect(()=>{
+  //   console.log("Me ejecute!!!");
+  // })
+  //Se ejecuta varias veces cuando se actualza el componente
 
-  return (
-    <div>
-      <p>Presionado: {conteo}</p>
-      <button onClick={()=>setCounter(conteo+1)}>Clickeame!!!!</button>
-    </div>
-  )
+  // useEffect(()=>{
+  //   console.log("Me ejecute!!!");
+  // },[])
+  //Se ejecuta solo al iniciar el componewnte
+  //Se eejcuta solo una vez, sie l componente se actualza esto no, se añade un arreglo al final 
+  
+  useEffect(()=>{
+    console.log("Me ejecute!!!");
+    return () => {
+      console.log("Adios!!!!");
+    }
+  },[]);
+  //Si la funcion retorna otra funcion, esa se ejecuta cuando se elimine el componente
+  
+  return <button onClick={()=>[setConteo(conteo+1)]}> Hacer CLICK JAJAJA</button>
+
 }
 
 const App = () => {
-  return <div><Button/></div>
+  const [showButton, setShowButton] = useState(false);
+  return (
+    <div>
+      <button onClick={ ()=> {setShowButton(false)} }>Eliminar boton</button>
+      <div>
+        {showButton && <Button/>}
+      </div>
+    </div>
+  )
 };
 
 render(<App />, document.getElementById('react-app'));
