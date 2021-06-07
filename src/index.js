f638f2808b1359c2fa2d2568a97d8b7dc03f509f
Kevin Cardenas <kscardenasa2013t@gmail.com>
2021-06-07 17:51:00 -0500

    Crear y capturar valores de un formulario

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludo = ()=>{
-  const [name, setName] = useState("");
+const Form = () =>{
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
 
   return (
-    <div>
-      <input type="text" onChange={(ev)=>{setName(ev.target.value)}}/>
-      <p>Hola {name}</p>
-    </div>
+    <form >
+      <div>
+        <label for='title'>Titulo </label>
+        <input type="text" id='title' onChange={(ev)=>{setTitle(ev.target.value)}}/>
+      </div>
+      <div>
+        <label for="body">Publicacion</label>
+        <textarea id="body" onChange={(ev)=>{setBody(ev.target.value)}}></textarea>
+      </div>
+      <input type="submit" value="Enviar" />
+    </form>
   )
 }
 
 const App = () => {
   return(
-    <Saludo></Saludo>
+    <Form></Form>
   );
 };
 
